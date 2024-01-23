899fc8ccb66f7cee1ee8b63d4162abe7162147b3
Than Zaw Toe <131885332+thanzawtoe888@users.noreply.github.com>
2024-01-24 01:22:15 +0630

    USB CDC protocol test finish Hi->Welcome feedback

---
+++ myWorkspace/myWS/USB_test00/MDK-ARM/USB_test00/startup_stm32f407xx.d
@@ -0,0 +1,1 @@
+usb_test00\startup_stm32f407xx.o: startup_stm32f407xx.s
---
+++ myWorkspace/myWS/Volatile_keyword/Objects/main.d
@@ -0,0 +1,2 @@
+./objects/main.o: main.c RTE\_Target_1\Pre_Include_Global.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
