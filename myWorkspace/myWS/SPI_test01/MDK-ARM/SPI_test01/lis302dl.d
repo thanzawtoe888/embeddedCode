0440456c347d39454645e0a8c6c5017c4ded15a4
Than Zaw Toe <131885332+thanzawtoe888@users.noreply.github.com>
2024-01-26 12:12:18 +0630

    SPI board support package BSP test Success

---
+++ myWorkspace/myWS/SPI_test01/MDK-ARM/SPI_test01/lis302dl.d
@@ -0,0 +1,4 @@
+spi_test01\lis302dl.o: lis302dl.c
+spi_test01\lis302dl.o: lis302dl.h
+spi_test01\lis302dl.o: accelero.h
+spi_test01\lis302dl.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ myWorkspace/myWS/SPI_test01/MDK-ARM/SPI_test01/lis3dsh.d
@@ -0,0 +1,4 @@
+spi_test01\lis3dsh.o: lis3dsh.c
+spi_test01\lis3dsh.o: lis3dsh.h
+spi_test01\lis3dsh.o: accelero.h
+spi_test01\lis3dsh.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
